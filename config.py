dccb55a1b51e12278865ab55791a43f88a64583c
peter parker <tanishqbokade555@gmail.com>
2025-06-07 06:15:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+API_ID = 26568356   # api id
+API_HASH = "271624eab37e854cbd9ae06a855f23e9"  # api hash
+
+BOT_TOKEN = "7898472921:AAFYErQ-_sfOp_Uhgn8cvU-t4-3IU90tqbo"  # bot token
+
+
+# REDIS
+HOST = "localhost"  # redis host uri
+PORT = 6379  # redis port
+PASSWORD = "osWvPw6FnZxJxL08zbwjLokwJCxXchwq"  # redis password
+
+PRIVATE_CHAT_ID = -1002547558208  # CHAT WHERE YOU WANT TO STORE VIDEOS
+# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
+COOKIE = ""
+ADMINS = [7841668686]
+
+
+BOT_USERNAME = "peter_pdf_bot"
+
+# Force user to join this channel. (make sure you have promoted the bot on this chat.)
+FORCE_LINK = "@carino_here"
+
+PUBLIC_EARN_API = ""  # https://publicearn.com/api
